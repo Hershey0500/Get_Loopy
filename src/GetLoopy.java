40410313a72b1130bccfe2e19ca9fb7b4c1f8da3
harsh <harshpatel05005@gmail.com>
2024-10-14 11:23:04 -0400

    Initial commit

---
+++ src/GetLoopy.java
@@ -0,0 +1,9 @@
+public class GetLoopy {
+    public static void main(String[] args) {
+        // Task 1: Count up by 1 from 0 to 30
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();  // For a new line
+    }
+}
